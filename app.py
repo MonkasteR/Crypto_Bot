b0287b94df9e317c666b04d98b48082716b50a94
MonkasteR <monkaster@gmail.com>
2023-07-08 14:50:45 +0300

    Промежуточный итог

--- app.py
+++ app.py
@@ -23,14 +23,19 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
-    values = message.text.split(' ')
-    if len(values) != 3:
-        raise ConvertionException('Неверное количество параметров.')
-    quote, base, amount = values
-    total_base = CriptoConverter.convert(quote, base, amount)
-
-    text = f'Цена {amount} {quote} в {base} = {total_base}'
-    bot.send_message(message.chat.id, text)
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConvertionException('Неверное количество параметров.')
+        quote, base, amount = values
+        total_base = CriptoConverter.convert(quote, base, amount)
+    except Exception as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message,f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} = {total_base}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
