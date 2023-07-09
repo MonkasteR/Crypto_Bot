aec711b40b50eb8e824eaa3f984a56112dbcd397
MonkasteR <monkaster@gmail.com>
2023-07-09 22:28:46 +0300

    Убрал из доступа конфиги

---
+++ config_me.py
@@ -0,0 +1,16 @@
+TOKEN = "YOUR_TELEGRAM_TOKEN"
+API_KEY = "YOUR_API_KEY"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфир': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'стейблкоин': 'USDT',
+    'бинанс': 'BNB',
+    'собака': 'DOGE',
+}
+'''
+Rename this file to config.py and put your data in it
+'''
